56edf2cb982109ff3485cf5c6190ad526c764ba1
Adam Ulrich <adamulrich@hotmail.com>
2023-01-02 20:03:32 -0800

    initial code commit

---
+++ phpmotors/css/snippets/navigation.php
@@ -0,0 +1,10 @@
+<nav id="page_nav">
+  <ul>
+    <li><a href="/">Home</a><li>
+    <li><a href="/Classic">Classic</a><li>
+    <li><a href="/Sports">Sports</a><li>
+    <li><a href="/SUV">SUV</a><li>
+    <li><a href="/Trucks">Trucks</a><li>
+    <li><a href="/Used">Used</a><li>
+  </ul>
+</nav>
---
+++ phpmotors/template.php
@@ -0,0 +1,3 @@
+<nav id="page_nav">
+ <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?> 
+</nav>
