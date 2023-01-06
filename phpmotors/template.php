46b8ba71e1b56a96184053b62a610008fd4811b0
Adam Ulrich <adamulrich@hotmail.com>
2023-01-05 20:21:18 -0800

    updated formatting

--- phpmotors/template.php
+++ phpmotors/template.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/head.php'; ?>    
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/head.php'; ?>
+
 <body>
     <!-- Header -->
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/header.php'; ?>    
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/header.php'; ?>
 
     <!-- Main -->
     <?php
-        $url = parse_url($_SERVER['REQUEST_URI']);
-        if (array_key_exists('query',$url)) 
-        {
-            parse_str($url['query'], $params);
-            if (array_key_exists('page',$params))
-            {
-                $current_page = $params['page'];
-            }
-            else 
-            {
-                $current_page = 'home';
-            }
-    
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    if (array_key_exists('query', $url)) {
+        parse_str($url['query'], $params);
+        if (array_key_exists('page', $params)) {
+            $current_page = $params['page'];
         } else {
-        $current_page = 'home';
+            $current_page = 'home';
         }
+    } else {
+        $current_page = 'home';
+    }
 
     echo $current_page;
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/main/' . $current_page . '.php'; 
-    ?>    
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/main/' . $current_page . '.php';
+    ?>
 
     <!-- Footer -->
     <footer>
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/footer.php'; ?> 
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/cse340/phpmotors/snippets/footer.php'; ?>
     </footer>
 </body>
+
 </html>
